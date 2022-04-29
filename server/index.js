45f930f92e8a5ae716322f212d1e890455dae8f7
JoeVanCamp <vancampjoe@gmail.com>
2022-04-28 20:48:24 -0700

    Updated env variables

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const app = require('./app')
 const PORT = 3000;
-
+console.log(PORT)
 app.listen(PORT, () => console.log(`Listening on port...${PORT}`))
+
+
+
--- server/models/databaseInteraction.js
+++ server/models/databaseInteraction.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 require('dotenv').config()
-
 const pool = new Pool({
   host: process.env.PGHOST,
   user: process.env.PGUSER,
-  database: process.env.PGDATABASE
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
 });
 
 
